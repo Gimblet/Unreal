c9ccb1f9296588344fe56064f82383452e247dd8
Gimblet <gimbletdev@gmail.com>
2023-11-04 10:23:29 -0500

    Header On Hover Actualizado
    Complete lo que sucede al poner el mouse sobre el header (Que cambien de color los iconos y las letras).
    
    Ademas se agregó comentarios a las funciones y se le cambió el nombre a estas para hacerlas mas legibles

--- JavaScript/src.js
+++ JavaScript/src.js
@@ -1,34 +1,60 @@
-//!Header
-
+//! ///////////////// Header /////////////////////
+//! Se encarga que al Hacer Hover en el Header, este cambie (Linea 3 -> Linea 30)
 function headerOnHover() {
-  changeHeaderBackgroundColor();
-  changeHeaderTextColorToBlack();
+  fondoEncabezadoABlanco();
+  textoEncabezadoANegro();
+  imagenesEncabezadoANegro();
 }
 
-function changeHeaderBackgroundColor() {
+function fondoEncabezadoABlanco() {
   return (
     document.querySelector("header").style.backgroundColor = "white",
     document.querySelector("header").style.transition = "all 0.35s linear"
   )
 }
 
-function changeHeaderTextColorToBlack() {
+function textoEncabezadoANegro() {
   let Textos = document.getElementsByClassName("color");
   for (i = 0; i < Textos.length; i++) {
     Textos[i].style.color = "black";
   }
 }
 
-function changeHeaderTextColorToWhite() {
+function imagenesEncabezadoANegro() {
+  document.getElementById("unrealLogo").setAttribute("src", "../Logos/Logo negro.png");
+  let iconos = document.getElementsByClassName("icon");
+  iconos[0].setAttribute("src", "../Iconos/Negro/Ncarrito-de-compras.png");
+  iconos[1].setAttribute("src", "../Iconos/Negro/Nusuario.png");
+  iconos[2].setAttribute("src", "../Iconos/Negro/Nbusqueda.png");
+  iconos[3].setAttribute("src", "../Iconos/Negro/Nmenu-hamburguesa.png");
+}
+
+//!Se encarga que al Hacer OnMouseOut en el Header, este cambie a como estaba originalmente (Linea 33 -> Linea 60)
+function headerOnMouseOut() {
+  fondoEncabezadoPorDefecto();
+  textoEncabezadoPorDefecto();
+  imagenesEncabezadoABlanco();
+}
+
+function fondoEncabezadoPorDefecto() {
+  return (
+    document.querySelector("header").style.backgroundColor = "transparent",
+    document.querySelector("header").style.transition = "all 0.35s linear"
+  )
+}
+
+function textoEncabezadoPorDefecto() {
   let Textos = document.getElementsByClassName("color");
   for (i = 0; i < Textos.length; i++) {
     Textos[i].style.color = "white";
   }
 }
 
-function headerOnMouseOut() {
-  document.querySelector("header").style.backgroundColor = "transparent";
-  changeHeaderTextColorToWhite();
+function imagenesEncabezadoABlanco() {
+  document.getElementById("unrealLogo").setAttribute("src", "../Logos/Logo blanco.png");
+  let iconos = document.getElementsByClassName("icon");
+  iconos[0].setAttribute("src", "../Iconos/carrito-de-compras.png");
+  iconos[1].setAttribute("src", "../Iconos/usuario (1).png");
+  iconos[2].setAttribute("src", "../Iconos/busqueda.png");
+  iconos[3].setAttribute("src", "../Iconos/menu-hamburguesa.png");
 }
-
-//Falta Cambiar las imagenes del Header on Hover y en MouseOut
