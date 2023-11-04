e54e4528e758ae017962f4afb0fc91e43cf83d09
Gimblet <gimbletdev@gmail.com>
2023-11-03 19:21:42 -0500

    Actualizar Header On Hover

--- JavaScript/src.js
+++ JavaScript/src.js
@@ -1,10 +1,34 @@
 //!Header
 
 function headerOnHover() {
-  document.querySelector("header").style.backgroundColor = "black";
-  document.querySelector("header").style.transition = "all 0.35s linear"
+  changeHeaderBackgroundColor();
+  changeHeaderTextColorToBlack();
+}
+
+function changeHeaderBackgroundColor() {
+  return (
+    document.querySelector("header").style.backgroundColor = "white",
+    document.querySelector("header").style.transition = "all 0.35s linear"
+  )
+}
+
+function changeHeaderTextColorToBlack() {
+  let Textos = document.getElementsByClassName("color");
+  for (i = 0; i < Textos.length; i++) {
+    Textos[i].style.color = "black";
+  }
+}
+
+function changeHeaderTextColorToWhite() {
+  let Textos = document.getElementsByClassName("color");
+  for (i = 0; i < Textos.length; i++) {
+    Textos[i].style.color = "white";
+  }
 }
 
 function headerOnMouseOut() {
   document.querySelector("header").style.backgroundColor = "transparent";
+  changeHeaderTextColorToWhite();
 }
+
+//Falta Cambiar las imagenes del Header on Hover y en MouseOut
