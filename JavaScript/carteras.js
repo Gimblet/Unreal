0f61e3d407c08cdc4ae5377c5b25d897e1c7a0d1
Gimblet <gimbletdev@gmail.com>
2023-11-12 00:54:12 -0500

    Agregar Función Cambio de Img
    En el javascript que tiene esta pagina exclusivamente se agregó una función que obtiene el src id en el que esta. Luego lo pone en array Auxiliar para luego ese arryay auxiliar se termine almacenando en una variable tipo String a la cual finalmente se le toma como src del elemento que se dio click. La base esta lista pero falta pulir ciertos detalles y hacer que funcione como tal. Si tengo tiempo mañana lo hago

--- JavaScript/carteras.js
+++ JavaScript/carteras.js
@@ -1,1 +1,35 @@
-//Proximamente
+//! ///////////// Cambiar Imagen Segun Click //////////
+function cambiarAPrimeraImagen() {
+  var rutaInicial = document.getElementById("Aux1").src;
+  var Aux1 = new Array(rutaInicial.length);
+  var rutaFinal = '';
+  var c = 0;
+
+  function crearArregloAuxiliar() {
+    for (i = 0; i < Aux1.length; i++) {
+      Aux1[i] = rutaInicial[i];
+    }
+  }
+  crearArregloAuxiliar();
+
+  function posicionNumeroImg() {
+    var pos = Aux1.length;
+    return pos - 4;
+  }
+
+  function obtenerRutaFinal() {
+    for (i = 0; i < Aux1.length; i++) {
+      c++;
+      var temp = Aux1[i];
+      if (posicionNumeroImg() == c) {
+        temp = "2";
+      }
+      rutaFinal = rutaFinal + temp;
+    }
+  }
+  obtenerRutaFinal();
+
+  document.getElementById("Aux1").setAttribute("src", rutaFinal);
+}
+
+document.getElementById("Aux1").addEventListener("click", cambiarAPrimeraImagen);
