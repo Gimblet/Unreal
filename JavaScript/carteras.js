edfaa67894060b69a61a5eacdbbccdaa9d9398ba
Gimblet <gimbletdev@gmail.com>
2023-11-12 11:24:30 -0500

    Mejora en la Claridad 2.0
    Se agregó una función para que el codigo sea mas facil de leer

--- JavaScript/carteras.js
+++ JavaScript/carteras.js
@@ -17,16 +17,18 @@ function cambiarImagenPrincipal(auxn) {
     return pos - 4;
   }
 
+  function obtenerTemp(temp) {
+    if (auxn == "Aux1") { return temp = "1"; }
+    else if (auxn == "Aux2") { return temp = "2"; }
+    else if (auxn == "Aux3") { return temp = "3"; }
+    else { return temp = "1"; }
+  }
+
   function obtenerRutaFinal() {
     for (i = 0; i < ArrayAux.length; i++) {
       c++;
       var temp = ArrayAux[i];
-      if (posicionNumeroImg() == c) {
-        if (auxn == "Aux1") { temp = "1"; }
-        else if (auxn == "Aux2") { temp = "2"; }
-        else if (auxn == "Aux3") { temp = "3"; }
-        else { temp = "1"; }
-      }
+      if (posicionNumeroImg() == c) { obtenerTemp(temp); }
       rutaFinal = rutaFinal + temp;
     }
   }
