1c87f5166f27e3c7b655d813fec506b36f68f670
Gimblet <gimbletdev@gmail.com>
2023-11-12 11:18:27 -0500

    Mejora en claridad de funcion
    Se cambiaron nombres de Variables e indentación para que sea mas facil de leer

--- JavaScript/carteras.js
+++ JavaScript/carteras.js
@@ -1,39 +1,31 @@
 //! ///////////////// Cambiar Imagen Segun Click /////////////
 function cambiarImagenPrincipal(auxn) {
   var rutaInicial = document.getElementById(auxn).src;
-  var Aux1 = new Array(rutaInicial.length);
+  var ArrayAux = new Array(rutaInicial.length);
   var rutaFinal = '';
   var c = 0;
 
   function crearArregloAuxiliar() {
-    for (i = 0; i < Aux1.length; i++) {
-      Aux1[i] = rutaInicial[i];
+    for (i = 0; i < ArrayAux.length; i++) {
+      ArrayAux[i] = rutaInicial[i];
     }
   }
   crearArregloAuxiliar();
 
   function posicionNumeroImg() {
-    var pos = Aux1.length;
+    var pos = ArrayAux.length;
     return pos - 4;
   }
 
   function obtenerRutaFinal() {
-    for (i = 0; i < Aux1.length; i++) {
+    for (i = 0; i < ArrayAux.length; i++) {
       c++;
-      var temp = Aux1[i];
+      var temp = ArrayAux[i];
       if (posicionNumeroImg() == c) {
-        if (auxn == "Aux1") {
-          temp = "1";
-        }
-        else if (auxn == "Aux2") {
-          temp = "2";
-        }
-        else if (auxn == "Aux3") {
-          temp = "3";
-        }
-        else {
-          temp = "1";
-        }
+        if (auxn == "Aux1") { temp = "1"; }
+        else if (auxn == "Aux2") { temp = "2"; }
+        else if (auxn == "Aux3") { temp = "3"; }
+        else { temp = "1"; }
       }
       rutaFinal = rutaFinal + temp;
     }
